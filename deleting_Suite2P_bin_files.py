de79f519684a140c3307acd663538383d396c282
Maria Cozan <maria.cozan@gmail.com>
2022-02-18 17:41:19 +0000

    Update deleting_Suite2P_bin_files.py

--- deleting_Suite2P_bin_files.py
+++ deleting_Suite2P_bin_files.py
@@ -7,4 +7,19 @@ Created on Fri Feb 18 16:46:53 2022
 
 import os
 
-os.remove("" )
+directory= "D://Suite2Pprocessedfiles"
+
+
+
+for file in directory if _.endswith(fileExt):
+    os.remove(files)
+    
+fileDir = r"C:\Test"
+fileExt = r".txt"
+_ for _ in os.listdir(fileDir) if _.endswith(fileExt)
+
+
+import glob
+
+targetPattern = r"C:\Te**\*.txt"
+glob.glob(targetPattern)
