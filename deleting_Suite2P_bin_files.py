81cf179f06ae6d78ea9a75d2fa72239087fbf988
Maria Cozan <maria.cozan@gmail.com>
2022-02-18 17:47:02 +0000

    Update deleting_Suite2P_bin_files.py

--- deleting_Suite2P_bin_files.py
+++ deleting_Suite2P_bin_files.py
@@ -10,10 +10,12 @@ import os
 directory= "D://Suite2Pprocessedfiles"
 
 
-
-for file in directory if _.endswith(fileExt):
-    os.remove(files)
+#for loop to go through the directory and delete the bin files
+for file in directory:
+    if file= "*.bin"
+    os.remove(file)
     
+#different options for choosing files with a certain extension obtained from the internet    
 fileDir = r"C:\Test"
 fileExt = r".txt"
 _ for _ in os.listdir(fileDir) if _.endswith(fileExt)
@@ -21,5 +23,7 @@ _ for _ in os.listdir(fileDir) if _.endswith(fileExt)
 
 import glob
 
-targetPattern = r"C:\Te**\*.txt"
-glob.glob(targetPattern)
+targetPattern = r"D://Suite2Pprocessedfiles//**//*.bin"
+
+for file in targetPattern:
+    os.remove(file)
