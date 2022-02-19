acb9ba385c5765f9f0f97845c9ea8a96c09e0068
Maria Cozan <maria.cozan@gmail.com>
2022-02-19 11:31:53 +0000

    Update deleting_Suite2P_bin_files.py

--- deleting_Suite2P_bin_files.py
+++ deleting_Suite2P_bin_files.py
@@ -7,19 +7,22 @@ Created on Fri Feb 18 16:46:53 2022
 
 import os
 
-directory= "D://Suite2Pprocessedfiles"
+#easy but a bit more time consuming
+os.remove("C://Temporary_Suite2P_output//suite2p//plane4//data.bin")
+os.remove("C://Temporary_Suite2P_output//suite2p//plane4//data_chan2.bin")
 
 
+#directory= "D://Suite2Pprocessedfiles"
 
-for file in directory if _.endswith(fileExt):
-    os.remove(files)
+# for file in directory if _.endswith(fileExt):
+#     os.remove(files)
     
-fileDir = r"C:\Test"
-fileExt = r".txt"
-_ for _ in os.listdir(fileDir) if _.endswith(fileExt)
+# fileDir = r"C:\Test"
+# fileExt = r".txt"
+# _ for _ in os.listdir(fileDir) if _.endswith(fileExt)
 
 
-import glob
+# import glob
 
-targetPattern = r"C:\Te**\*.txt"
-glob.glob(targetPattern)
+# targetPattern = r"C:\Te**\*.txt"
+# glob.glob(targetPattern)
