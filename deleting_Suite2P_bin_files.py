dd18addaf55c5f9fd88bd80347a3f6d33f7fc61f
Maria Cozan <maria.cozan@gmail.com>
2022-02-21 17:14:19 +0000

    Update deleting_Suite2P_bin_files.py

--- deleting_Suite2P_bin_files.py
+++ deleting_Suite2P_bin_files.py
@@ -8,38 +8,38 @@ Created on Fri Feb 18 16:46:53 2022
 import os
 
 #easy but a bit more time consuming
-os.remove("C://Temporary_Suite2P_output//suite2p//plane4//data.bin")
-os.remove("C://Temporary_Suite2P_output//suite2p//plane4//data_chan2.bin")
+os.remove("C://Temporary_Suite2P_output//2022-02-21//suite2p//plane4//data.bin")
+os.remove("C://Temporary_Suite2P_output//2022-02-21//suite2p//plane4//data_chan2.bin")
 
 
 
-#directory= "D://Suite2Pprocessedfiles"
+# #directory= "D://Suite2Pprocessedfiles"
 
-# for file in directory if _.endswith(fileExt):
-#     os.remove(files)
+# # for file in directory if _.endswith(fileExt):
+# #     os.remove(files)
     
+# # fileDir = r"C:\Test"
+# # fileExt = r".txt"
+# # _ for _ in os.listdir(fileDir) if _.endswith(fileExt)
+# #for loop to go through the directory and delete the bin files
+# for file in directory:
+#     if file= "*.bin"
+#     os.remove(file)
+    
+# #different options for choosing files with a certain extension obtained from the internet    
 # fileDir = r"C:\Test"
 # fileExt = r".txt"
 # _ for _ in os.listdir(fileDir) if _.endswith(fileExt)
-#for loop to go through the directory and delete the bin files
-for file in directory:
-    if file= "*.bin"
-    os.remove(file)
-    
-#different options for choosing files with a certain extension obtained from the internet    
-fileDir = r"C:\Test"
-fileExt = r".txt"
-_ for _ in os.listdir(fileDir) if _.endswith(fileExt)
 
 
 
-# import glob
+# # import glob
 
 
-# targetPattern = r"C:\Te**\*.txt"
-# glob.glob(targetPattern)
+# # targetPattern = r"C:\Te**\*.txt"
+# # glob.glob(targetPattern)
 
-targetPattern = r"D://Suite2Pprocessedfiles//**//*.bin"
+# targetPattern = r"D://Suite2Pprocessedfiles//**//*.bin"
 
-for file in targetPattern:
-    os.remove(file)
+# for file in targetPattern:
+#     os.remove(file)
