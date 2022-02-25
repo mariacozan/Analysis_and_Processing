134606e58d5cca180ac9cf1ae6222573ddf5075a
Maria Cozan <maria.cozan@gmail.com>
2022-02-25 17:32:47 +0000

    v

--- 2022-02-15_loading_and_plotting_traces.py
+++ 2022-02-15_loading_and_plotting_traces.py
@@ -1,30 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#use seaborne in the future!!
+import seaborn as sns
+#use seaborn in the future!!
 
 #normalizing the array
 def normalise(data):
     return ((data - np.min(data))) / ((np.max(data) - np.min(data)))
 
 #adjust file locations as needed, there will be the same set of data for every plane
-#F = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//F.npy', allow_pickle=True)
+F = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//F.npy', allow_pickle=True)
 #F_chan2 = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//F_chan2.npy', allow_pickle=True)
-#Fneu = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//Fneu.npy', allow_pickle=True)
+Fneu = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//Fneu.npy', allow_pickle=True)
 #spks = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//spks.npy', allow_pickle=True)
 #stat = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//stat.npy', allow_pickle=True)
-ops =  np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//ops.npy', allow_pickle=True)
-ops = ops.item()
+#ops =  np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//ops.npy', allow_pickle=True)
+#ops = ops.item()
 # iscell = np.load('D:/Suite2Pprocessedfiles/SS057/20220112/suite2p/combined//iscell.npy', allow_pickle=True)
+
 
 # subtracted= F-Fneu
 # # plt.imshow(subtracted)
 
-# # fig, axs = plt.subplots(1, 2, figsize=(30, 9))
-# # axs[0].plot(F[13])
-# # axs[1].plot(Fneu[13])
+# fig, axs = plt.subplots(1, 2, figsize=(30, 9))
+# axs[0].plot(F[31])
+# axs[1].plot(Fneu[31])
 
-# #plt.plot(F[13])
-# #plt.plot(Fneu[13])
+plt.plot(F[390])
+plt.plot(Fneu[390])
+
+    
+
+
+#sns.relplot(data=np.transpose(F), kind="line", height=5,legend=None, aspect=2)
+
+#sns.relplot(data=np.transpose(Fneu), kind="line", height=5,legend=None, aspect=2)
 
 # norm_F= normalise(F)
 
@@ -33,7 +42,7 @@ ops = ops.item()
 # for i in range(1,n):
 #     plt.plot(F[i])
 
-# #plt.savefig('D://Suite2Pprocessedfiles//SS057//20220112//plots//n10.png')
+plt.savefig('D://Figures//SS057//ROI_390.png')
 
 # #plot deconvolution
 # # for i in range(1,10):
