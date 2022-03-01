12163c2a4d8ef4f3904f35946cca71a52bef8827
Maria Cozan <maria.cozan@gmail.com>
2022-03-01 16:47:56 +0000

    .

--- 2022-02-15_loading_and_plotting_traces.py
+++ 2022-02-15_loading_and_plotting_traces.py
@@ -8,13 +8,15 @@ def normalise(data):
     return ((data - np.min(data))) / ((np.max(data) - np.min(data)))
 
 #adjust file locations as needed, there will be the same set of data for every plane
-F = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//F.npy', allow_pickle=True)
+#F = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//F.npy', allow_pickle=True)
 #F_chan2 = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//F_chan2.npy', allow_pickle=True)
-Fneu = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//Fneu.npy', allow_pickle=True)
+#Fneu = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//Fneu.npy', allow_pickle=True)
 #spks = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//spks.npy', allow_pickle=True)
 #stat = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//stat.npy', allow_pickle=True)
-#ops =  np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//ops.npy', allow_pickle=True)
-#ops = ops.item()
+ops =  np.load('D://Suite2Pprocessedfiles//Eos//20220228//suite2p//combined//ops.npy', allow_pickle=True)
+ops = ops.item()
+ops =  np.load('C://Temporary_Suite2P_output//Eos//20220228//suite2p//combined//ops.npy', allow_pickle=True)
+ops = ops.item()
 # iscell = np.load('D:/Suite2Pprocessedfiles/SS057/20220112/suite2p/combined//iscell.npy', allow_pickle=True)
 
 
@@ -25,8 +27,8 @@ Fneu = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//Fne
 # axs[0].plot(F[31])
 # axs[1].plot(Fneu[31])
 
-plt.plot(F[390])
-plt.plot(Fneu[390])
+# plt.plot(F[390])
+# plt.plot(Fneu[390])
 
     
 
@@ -42,7 +44,7 @@ plt.plot(Fneu[390])
 # for i in range(1,n):
 #     plt.plot(F[i])
 
-plt.savefig('D://Figures//SS057//ROI_390.png')
+#plt.savefig('D://Figures//SS057//ROI_390.png')
 
 # #plot deconvolution
 # # for i in range(1,10):
