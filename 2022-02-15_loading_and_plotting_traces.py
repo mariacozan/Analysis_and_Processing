c60d7bc3d5279547e43296b7486f1ac38d4f5dc6
Maria Cozan <maria.cozan@gmail.com>
2022-03-04 18:08:48 +0000

    r

--- 2022-02-15_loading_and_plotting_traces.py
+++ 2022-02-15_loading_and_plotting_traces.py
@@ -8,15 +8,18 @@ def normalise(data):
     return ((data - np.min(data))) / ((np.max(data) - np.min(data)))
 
 #adjust file locations as needed, there will be the same set of data for every plane
-#F = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//F.npy', allow_pickle=True)
+# F1 = np.load('D://Suite2Pprocessedfiles//Eos//2022-03-04//te2p//plane0//F.npy', allow_pickle=True)
+F1= np.load('D://Suite2Pprocessedfiles//Eos//2022-03-04//no_neuropil//512/suite2p//plane0//F.npy', allow_pickle=True)
+
+F2= np.load('D://Suite2Pprocessedfiles//Eos//2022-03-04//no_neuropil//256//suite2p//plane0//F.npy', allow_pickle=True)
 #F_chan2 = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//F_chan2.npy', allow_pickle=True)
 #Fneu = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//plane1//Fneu.npy', allow_pickle=True)
 #spks = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//spks.npy', allow_pickle=True)
 #stat = np.load('D://Suite2Pprocessedfiles//SS057//20220112//suite2p//combined//stat.npy', allow_pickle=True)
-ops =  np.load('D://Suite2Pprocessedfiles//Eos//20220228//suite2p//combined//ops.npy', allow_pickle=True)
-ops = ops.item()
-ops =  np.load('C://Temporary_Suite2P_output//Eos//20220228//suite2p//combined//ops.npy', allow_pickle=True)
-ops = ops.item()
+#ops =  np.load('D://Suite2Pprocessedfiles//Eos//20220228//suite2p//combined//ops.npy', allow_pickle=True)
+#ops = ops.item()
+#ops =  np.load('C://Temporary_Suite2P_output//Eos//20220228//suite2p//combined//ops.npy', allow_pickle=True)
+#ops = ops.item()
 # iscell = np.load('D:/Suite2Pprocessedfiles/SS057/20220112/suite2p/combined//iscell.npy', allow_pickle=True)
 
 
@@ -27,12 +30,23 @@ ops = ops.item()
 # axs[0].plot(F[31])
 # axs[1].plot(Fneu[31])
 
-# plt.plot(F[390])
-# plt.plot(Fneu[390])
+# # choosing one ROI
+# n=2
+# one_ROI= (F1[n])
+# #converting frames to seconds
+# fs=6
 
-    
+ROI_256 = np.array(F1[5])
+ROI_512 = np.array(F2[21])
 
 
+#plotting one cell/ a few cells and comparing two analyses
+# plt.plot(F1[5], c="r")
+# plt.plot (F2[21], c="b")
+plt.plot(np.array(range(len(F1[10])))/6,F1[10], c="r")
+plt.plot(np.array(range(len(F2[0])))/7.27,F2[0], c="b")
+
+# x_256= np.array(range(len(F1[5])))/3
 #sns.relplot(data=np.transpose(F), kind="line", height=5,legend=None, aspect=2)
 
 #sns.relplot(data=np.transpose(Fneu), kind="line", height=5,legend=None, aspect=2)
