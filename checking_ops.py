6484fed872d307785d80e9b9143769627eeb19e8
Maria Cozan <maria.cozan@gmail.com>
2022-03-22 16:31:48 +0000

    checking ops made easier

---
+++ checking_ops.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 16:07:57 2022
+
+@author: maria
+"""
+
+import numpy as np
+
+#checking the ops details of files
+animal=  'Hedes'
+date= '2022-03-17'
+#note: if experiment type not known, put 'suite2p' instead
+experiment= 'SparseNoise'
+
+filePath='D://Suite2Pprocessedfiles//'+animal+ '//'+date+ '//2022-03-17data//'+experiment+ '//plane1//ops.npy'
+
+ops =  np.load(filePath, allow_pickle=True)
+ops = ops.item()
+
+#printing data path to know which data was analysed
+key_list = list(ops.values())
+print(key_list[88])
