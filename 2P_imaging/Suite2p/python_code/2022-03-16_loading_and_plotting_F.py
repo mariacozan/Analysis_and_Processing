2e9606bc1d497c569d610a6c4b713659431577ff
Maria Cozan <83554323+mariacozan@users.noreply.github.com>
2022-04-28 13:37:42 +0100

    Update 2022-03-16_loading_and_plotting_F.py

--- 2P_imaging/Suite2p/python_code/2022-03-16_loading_and_plotting_F.py
+++ 2P_imaging/Suite2p/python_code/2022-03-16_loading_and_plotting_F.py
@@ -14,7 +14,7 @@ F= np.load('C:/Users/maria/Documents/GitHub/internal/2P_imaging/Suite2p/Suite2P_
 Fneu= np.load('C:/Users/maria/Documents/GitHub/internal/2P_imaging/Suite2p/Suite2P_output/Hedes//2022-03-23//Fneu.npy', allow_pickle=True)
 ops= np.load('C:/Users/maria/Documents/GitHub/internal/2P_imaging/Suite2p/Suite2P_output/Hedes//2022-03-23//ops.npy', allow_pickle=True)
 ops= ops.item()
-#for this file (Eos 2022-03-09), the ROIs that appear to respond to the gratings are: 0, 1, 3, 4, 6, 8, 10, 11, 12, 25, 26 
+
 
 #plotting an example trace
 #n is the ROI you want to plot
@@ -25,4 +25,4 @@ fr= 6
 plt.plot(np.array(range(len(F[n])))/fr,F[n], c="b")
 plt.plot(np.array(range(len(F[n])))/fr,Fneu[n], c="r")
 plt.xlabel("Time(s)")
-plt.ylabel("Flurescence Intensity")
+plt.ylabel("Fluorescence Intensity")
